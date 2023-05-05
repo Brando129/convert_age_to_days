bb421d416ae473b85ee93abdbe5f42a5fddd5c8b
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-05 09:51:40 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create a function that takes the age in years and returns the age in days.
+
+function convertAge(age){
+    var days = age * 365
+    return days
+}
+
+console.log(convertAge(2))
